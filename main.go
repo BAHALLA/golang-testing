27e8af80822d18a4e46825d93761069c4dfe875a
Taoufiq BAHALLA <taoufiq.bahalla@hotmail.com>
2023-12-27 22:01:08 +0100

    Get number from user input

--- main.go
+++ main.go
@@ -1,14 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
+	intro()
 
-	n := 7
+	doneChan := make(chan bool)
 
-	_, msg := isPrime(n)
+	go readUserInput(doneChan)
 
-	fmt.Println(msg)
+	<-doneChan
+
+	close(doneChan)
+
+	fmt.Println("Goodbye !")
+
+}
+
+func intro() {
+
+	fmt.Println("Is it Prime number ? ")
+	fmt.Println("--------------------")
+	fmt.Println("Enter a number, and i will tell you if it is a prime one !, Enter q to quit.")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("=> ")
+}
+
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+
+	scanner.Scan()
+
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	num, err := strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		return "please enter a number", false
+	}
+
+	_, msg := isPrime(num)
+
+	return msg, false
 }
 
 func isPrime(n int) (bool, string) {
